62973bde6325f6f00297a8dbe2377bc2b773584b
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-02-10 20:12:37 +0600

    added modal and dropdown componants

--- src/components/todo/TodoConttainer.tsx
+++ src/components/todo/TodoConttainer.tsx
@@ -1,22 +1,26 @@
+import { Button } from "../ui/button";
+import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 
 const TodoConttainer = () => {
   return (
     <div>
-      <div>
-        <button>Add Todo</button>
-        <button>Filter</button>
+      <div className="flex justify-between mb-5">
+        <AddTodoModal></AddTodoModal>
+        <Button>Filter</Button>
       </div>
-      <div className="bg-red-500 w-full rounded-xl p-5 space-y-3 ">
-        <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center rounded">
-          <p>There is no tusk pending</p>
-          {""}
+      <div className="bg-primary-gradient w-full rounded-xl space-y-3 p-[5px] ">
+        <div className="bg-white p-5 w-full h-full space-y-3 rounded-md">
+          <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center rounded">
+            <p>There is no tusk pending</p>
+            {""}
+          </div>
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
         </div>
-        <TodoCard />
-        <TodoCard />
-        <TodoCard />
-        <TodoCard />
-        <TodoCard />
       </div>
     </div>
   );
